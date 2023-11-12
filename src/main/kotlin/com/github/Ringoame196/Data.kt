ab1fef320716651d6a87404f49991ce57934f4d1
りんご飴196 <kazumac41@gmail.com>
2023-11-12 20:31:03 +0900

    スマートフォン前

--- src/main/kotlin/com/github/Ringoame196/Data.kt
+++ src/main/kotlin/com/github/Ringoame196/Data.kt
@@ -1,7 +1,10 @@
 package com.github.Ringoame196
 
+import java.util.*
+
 class Data {
     object DataManager {
         var cheatWebhook: String? = null
+        val playerDataMap: MutableMap<UUID, PlayerData> = mutableMapOf()
     }
 }
---
+++ src/main/kotlin/com/github/Ringoame196/PlayerData.kt
@@ -0,0 +1,5 @@
+package com.github.Ringoame196
+
+data class PlayerData(
+    val smartphone: MutableList<String>
+)
